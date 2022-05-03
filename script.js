183fed84e8b8cae5dded70c77c88befc6e1e5258
Maple Chou <mapleelpam@gmail.com>
2022-05-03 10:27:47 +0800

    add unknown old bush

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Code goes here
 
-console.log ( "first ");
-function update_body( lang = "en", show_price = false ) {
+function update_body( lang = "en", show_full_name = false, show_price = false ) {
 	var table_string = "";
 
 	$.getJSON("db.json"). then( function(json) {
@@ -12,12 +11,14 @@ function update_body( lang = "en", show_price = false ) {
 				"<th> 型態 </th> \n" + 
 				"<th> 市/縣 </th> <th> 產地 </th> <th> 環境 </th> <th>克/盒</th> \n";
 			if( show_price ) { table_string += "<th>USD/Box</th>"; }
-			table_string +=  "<th> 節氣 </th>\n"+
-				" <th>採摘/制作</th><th>Instagram</th> </thead> \n <tbody> ";
+			table_string +=  "<th> 節氣 </th>\n"+ " <th>採摘/制作</th>";
+			if(show_fullname == "true" ) table_string += "<th>FullName</th>";
+			table_string +=  "<th>Instagram</th> </thead> \n <tbody> ";
 
 			var pt_dict = { "HongCha": "紅茶",
 					"BaoZhong": "包種",
 					"CharcoalBaoZhong": "炭焙包種",
+					"CharcoalTGY": "炭焙鐵觀音",
 					"BianCha": "扁茶",
 					"Green": "綠茶",
 					"OrientalBeauty":"白毫烏龍" };
@@ -25,8 +26,8 @@ function update_body( lang = "en", show_price = false ) {
 			var env_dict ={ "Wild": "野放",
 					"SemiWild": "半野",
 					"PureWild": "純野",
-					"WildAndOldBush": "野放老欉",
-					"WildAndTallBush": "野放高欉"
+					"WildOldBush": "野放老欉",
+					"WildTallBush": "野放高欉"
 					 };
 
 			for( var idx in json ) {
@@ -54,6 +55,12 @@ function update_body( lang = "en", show_price = false ) {
 				table_string +=  "<td>" + json[idx]["solar_term"] + " </td> \n";
 				table_string +=  "<td>" + json[idx]["harvest_date"] + " </td> \n";
 
+				if( show_fullname == "true" ){
+					table_string +=  "<td>" + json[idx]["harvest_date"] +"-"+json[idx]["solar_term"] +"-"+json[idx]["harvest_city_zh"]+"-"+json[idx]["harvest_area_zh"]
+						+ "-" + json[idx]["cultivar_zh"] + "-" + env_dict[json[idx]["cleaness"]] + "-" + pt_dict[json[idx]["process_type"]]
+						+ " </td> \n";
+				}
+
 				if( json[idx]["instagram_url"] == null ) {
 					table_string +=  "<td> not avaliable</td>\n";
 				} else {
@@ -71,9 +78,10 @@ function update_body( lang = "en", show_price = false ) {
 				"<th>Cultivar Name </th> \n" + 
 				"<th>Type</th> <th>City</th> <th>From</th> <th>Enviroment</th> <th>Gram/Box</th>";
 
-			if( show_price ) { table_string += "<th>USD/Box</th>"; }
-
-			table_string += "<th>Harvest</th><th>Instagram</th> </thead> \n <tbody> ";
+			if( show_price ) { table_string += "<th>USD/Box</th>"; } 
+			table_string += "<th>Harvest</th>"; 
+			if(show_fullname == "true" ) table_string += "<th>FullName</th>";
+			table_string +=	"<th>Instagram</th> </thead> \n <tbody> ";
 
 			for( var idx in json ) {
 
@@ -98,6 +106,13 @@ function update_body( lang = "en", show_price = false ) {
 				}
 
 				table_string +=  "<td>" + json[idx]["harvest_date"] + " </td> \n";
+
+
+				if( show_fullname == "true" ){
+					table_string +=  "<td>" + json[idx]["harvest_date"] +"-"+json[idx]["harvest_city_en"]+"-"+json[idx]["harvest_area_en"]
+						+ "-" + json[idx]["cultivar_en"] + "-" + json[idx]["cleaness"] + "-" + json[idx]["process_type"] 
+						+ " </td> \n";
+				}
 
 				if( json[idx]["instagram_url"] == null ) {
 					table_string +=  "<td> N/A </td>\n";
@@ -127,17 +142,16 @@ function update_body( lang = "en", show_price = false ) {
 			//this._show_price = false;
 			if( $("#keyword").val() == "teamapletw" ) {
 				this._show_price = true;
-				update_body( lang, this._show_price );
+				update_body( lang, show_fullname, this._show_price );
 			} else if(this. _show_price == true ) {
 				this._show_price = false;
-				update_body( lang, this._show_price ); 
+				update_body( lang, show_fullname, this._show_price ); 
 			}
 		});
 	});
 }
 
 const urlParams = new URLSearchParams(window.location.search);
-console.log ( "hello " + urlParams );
-console.log ( "hey ");
 const lang = urlParams.get('lang');
-update_body( lang );
+const show_fullname= urlParams.get('fullname');
+update_body( lang, show_fullname, false );
